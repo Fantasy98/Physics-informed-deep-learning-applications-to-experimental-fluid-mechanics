68a6446ac539eb6ec8498a137cc8acc25c99635c
Yuning Wang <yuningw@mech.kth.se>
2024-02-10 12:29:13 +0100

    Update scalling

--- mi_channel/eval/b2_psd.py
+++ mi_channel/eval/b2_psd.py
@@ -19,23 +19,29 @@ def PSD_1D(data,Nx,Ny,Nz, nt,Lx,Ly,Lz,utype=0):
     Re_Tau = 202 #Direct from simulation
     Re = 5000 #Direct from simulation
     nu = 1/Re #Kinematic viscosity
-    # u_tau = Re_Tau*nu
+    u_tau = Re_Tau*nu
+    
+    eta = nu / u_tau
+    
     # eta = 1/Re**-0.75
-    eta = 1
-    nu =1
-    u_tau =1 
+    # eta = 1
+    # nu =1
+    # u_tau =1 
     yp =  30
-    y_loc = Ly * (yp/Ny)
+    y_loc = Ly * (yp/Ny) /eta
+
     print(f"At y = {y_loc}")
     # print(yp)
 
+    xp = int(Nx/2)
     # utype = 2
     # Streamwise velocity at wall
-    data = data[utype]
-    for t in range(nt):
-        data[:,:,:,t] = data[:,:,:,t] - data.mean(-1)
+    data = data[utype] 
+    # for t in range(nt):
+    #     data[:,:,:,t] = data[:,:,:,t] - data.mean(-1)
 
     data    = data[:,yp,:,:]
+    data    = data[:,:,xp,:]
     # Fluctuation
     # Wavenumber spacing
     dkx = 2*np.pi/(Lx) 
@@ -47,32 +53,31 @@ def PSD_1D(data,Nx,Ny,Nz, nt,Lx,Ly,Lz,utype=0):
     y_range =  dky *  np.linspace(-Ny/2 , Ny/2 -1, Ny)
     z_range =  dkz *  np.linspace(-Nz/2 , Nz/2 -1, Nz)
 
-    kx = dkx * np.append(x_range[:Nx//2], -x_range[Nx//2:0:-1])
-    ky = dky * np.append(y_range[:Ny//2], -y_range[Ny//2:0:-1])
-    kz = dkz * np.append(z_range[:Nz//2], -z_range[Nz//2:0:-1])
+    kx = np.append(x_range[:Nx//2], -x_range[Nx//2:0:-1])
+    ky = np.append(y_range[:Ny//2], -y_range[Ny//2:0:-1])
+    kz = np.append(z_range[:Nz//2], -z_range[Nz//2:0:-1])
 
     # kx = np.sqrt((kx)**2)
     # ky = np.sqrt((ky)**2)
     # kz = np.sqrt((kz)**2)
 
-    Lambda_x = kx * eta
-    Lambda_y = ky * eta
-    Lambda_z = kz * eta
+    Lambda_x = kx 
+    Lambda_y = ky 
+    Lambda_z = kz 
     spectra = np.empty(shape=(Nx,nt))
+    # spectra = np.empty(shape=(Ny,nt))
     for t in range(nt):
         # At each timestep, fft on x-z plane
         u_hat = np.fft.fftn(data[:,:,t])
         u_hat = np.fft.fftshift(u_hat)
         eng   = np.absolute(u_hat.mean(0))**2
-        spectra[:,t] = eng /(nu * u_tau)
+        spectra[:,t] = eng
         # spectra[:,t] = eng 
 
     # spectra = spectra/spectra.max()
     spectra = np.mean(spectra,-1)
-    # spectra = spectra[:,0]
-    # Lambda_x = np.fft.fftshift(Lambda_x)
-
-    return spectra, Lambda_x
+    wvnumber = Lambda_x * eta
+    return spectra, wvnumber
 
 
 
@@ -211,10 +216,12 @@ for utype in range(3):
             )
 
     axs.set_ylabel(Ylabels[utype],font_dict )
-    # axs.set_xlabel(r"$k_x \eta$", font_dict)   
-    axs.set_xlabel(r"$k_x$", font_dict)   
+    axs.set_xlabel(r"$k_x l^*$", font_dict)   
+    # axs.set_xlabel(r"$k_{y^+}$", font_dict)   
+    # axs.set_xlabel(r"$k_{y^+}$", font_dict)   
+    
     axs.set_ylim(5 * 10e-7, 10e0)
-    # axs.set_ylim(10e-2, 10e5)
+    
     axs.legend(frameon=False, ncol = 3, loc = (0.0, 1.05), fontsize=13)
 
     # axs.set_xticks([wvnumber.min(), 0.5 * wvnumber.max(), wvnumber.max()])
